672e9c31c1d0d0af7df535a74fd4a4ff3cc1bd98
Dhie_92 <115655470+titink8@users.noreply.github.com>
2022-12-03 06:30:25 +0700

    Create pop.js

---
+++ pop.js
@@ -0,0 +1,63 @@
+/*!
+ * POPjs JavaScript Library V1.0.1
+ * License: MIT
+ * File: POP.js
+ * Date: 2020-02-29T23:06+08:00
+ */
+function POPPage(underUrl,url,settings){
+  this.version="1.0.0";
+  if(settings){
+    if(settings["postback"]){
+      this.postback="?ok="+settings["postback"]
+    }else{
+      this.postback="?ok=popunder"
+    }
+    if(settings["point"]){
+      this.hash="#"+settings["point"]
+    }else{
+      this.hash="#"
+    }
+  }
+  
+  //bottom URL
+  this.underUrl=underUrl;
+  
+  //top URL
+  if(!url){
+    this.url=(location.href.replace(location.search,""))+this.postback+this.hash
+  }else{
+    this.url=url+this.postback+this.hash
+  }
+  
+}
+POPPage.prototype.under=function(){
+  //now page
+  var h=window.open(this.url,"_blank")
+  //under page
+  location.href=this.underUrl
+}
+POPPage.prototype.up=function(){
+  //now page
+  var h=window.open(this.underUrl,"_blank")
+  //under page
+  location.href=this.url
+}
+POPPage.prototype.postbackHandler=function(S){
+  var para=new URLSearchParams(location.search);
+  if(S){
+    for(var prop in S){
+      if(para.get("ok")==prop){
+        S[prop]();
+      }
+    }
+  }
+}
+
+window.POPPage=POPPage
+window.pop=function(t,s,set){
+  if(set){
+    return new POPPage(t,s,set);
+  }else{
+    return new POPPage(t,s);
+  }
+}
